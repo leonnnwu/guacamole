51f286dd9ddf6377d4d4f4d5b36cd14225931bf4
lwu <liang_wu@intuit.com>
2015-11-24 14:06:38 -0800

    Refactored testing method to make it common for other sorting tests.

--- src/com/lwu/algo/sort/test/SortTest.java
+++ src/com/lwu/algo/sort/test/SortTest.java
@@ -4,21 +4,26 @@ import com.lwu.algo.sort.main.InsertionSort;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 /**
  * Created by lwu on 11/24/15.
  */
-public class InsertionSortTest {
+public class SortTest {
 
     public static final Integer[] input = {1, 4, 3, 5, 6, 2};
     public static final Integer[] output = {1,2,3,4,5,6};
 
     @Test
-    public void testSort() throws Exception {
+    public void testInsertionSort() throws Exception {
         InsertionSort insertionSort = new InsertionSort();
-        insertionSort.sort(input);
 
-        for(int i=0; i<input.length; i++) {
-            Assert.assertEquals(output[i], input[i]);
+        Integer[] localArray = Arrays.copyOf(input, input.length);
+
+        insertionSort.sort(localArray);
+
+        for(int i=0; i<localArray.length; i++) {
+            Assert.assertEquals(output[i], localArray[i]);
         }
     }
 }
