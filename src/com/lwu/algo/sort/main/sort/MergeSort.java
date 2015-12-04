1a2ba4b26bfe4f35e3c7e3db5fd3d9eab6e31573
leonnnwu <wuliang0306@gmail.com>
2015-12-03 20:37:38 -0800

    Add Top-down MergeSort

--- src/com/lwu/algo/sort/main/sort/MergeSort.java
+++ src/com/lwu/algo/sort/main/sort/MergeSort.java
@@ -1,5 +1,7 @@
 package com.lwu.algo.sort.main.sort;
 
+import java.util.Arrays;
+
 /**
  * Stable:  Yes
  * Extra Space: O(n)
@@ -11,24 +13,46 @@ public class MergeSort extends SortBase{
 
     @Override
     public void sort(Comparable[] a) {
-        splitAndMerge(a, 0, a.length-1, new Comparable[a.length]);
+        splitAndMerge(a, 0, a.length-1);
     }
 
-    private void splitAndMerge(Comparable[] a, int start, int end, Comparable[] helper) {
-        if(end-start < 2) {
+    /**
+     * Top down split and merge
+     * @param a
+     * @param start
+     * @param end
+     */
+    private void splitAndMerge(Comparable[] a, int start, int end) {
+        if(start >= end) {
             return;
         }
 
-        int middle = (end - start)/2;
+        int middle = (end - start)/2 + start;
 
-        splitAndMerge(a, start, middle+start, helper);
-        splitAndMerge(a, middle+start+1, end, helper);
+        splitAndMerge(a, start, middle);
+        splitAndMerge(a, middle+1, end);
 
-        merge(a, start, middle+start, end, helper);
+        merge(a, start, middle, end);
     }
 
-    private void merge(Comparable[] a, int start, int middle, int end, Comparable[] helper) {
-        while(start < middle)
+    private void merge(Comparable[] a, int start, int middle, int end) {
+        int i = start;
+        int j = middle+1;
+        int k = start;
+
+        Comparable[] helper = Arrays.copyOf(a, a.length);
+
+        while(i <= middle && j <= end) {
+            a[k++] = less(helper[i], helper[j]) ? helper[i++] : helper[j++];
+        }
+
+        while(i <= middle) {
+            a[k++] = helper[i++];
+        }
+
+        while (j <= end) {
+            a[k++] = helper[j++];
+        }
     }
 
 }
